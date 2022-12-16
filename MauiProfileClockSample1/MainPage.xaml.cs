5172cfd740c9233c03352bd16eedd1817c49c8a0
{N.DZL} <cxnt48@gmail.com>
2022-12-16 12:45:02 +0100

    Add project files.

---
+++ MauiProfileClockSample1/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace MauiProfileClockSample1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	static MainPage _mainPage = null;
+	MainActivity _mainActivity = null;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		_mainPage = this;
+    }
+
+	public static MainPage getInstance()
+	{
+        return _mainPage;
+	}
+
+	public void setMainActivity(MainActivity mainActivity)
+	{
+        _mainPage._mainActivity = mainActivity;
+    }
+
+    private void onSetClockClicked(object sender, EventArgs e)
+    {
+		_mainActivity.onClickSetProfile(txtTimeZone.Text, txtDate.Text, txtTime.Text);
+    }
+
+	public void updateStatusText(string message)
+	{
+        lblStatus.Text = "Status: " + message;
+    }
+}
+
